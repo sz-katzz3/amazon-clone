821b7c0a9187b2f274fa3830bd56e91d2da4a2ac
kat <katavamp@gmail.com>
2020-10-07 16:09:41 +0900

    firstcommit

---
+++ src/Checkout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Checkout.css";
+
+function Checkout() {
+    return (
+        <div className='checkout'>
+            <div className="checkout__left">
+            <img className="checkout__ad" src="https://images-na-ssl-images-amazon.com/images/G/02/UK_CCMP/TM/0CC_Amazon1._CB423492668_.jpg" alt="" />
+            <h2 className="checkout">Your shopping Basket</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Checkout
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBUnFrIvhrADXtd5Z_aXAIZBnKkhJquUss",
+    authDomain: "challenge-9b9bf.firebaseapp.com",
+    databaseURL: "https://challenge-9b9bf.firebaseio.com",
+    projectId: "challenge-9b9bf",
+    storageBucket: "challenge-9b9bf.appspot.com",
+    messagingSenderId: "43280070805",
+    appId: "1:43280070805:web:ec811e8af1dca03e10439c",
+    measurementId: "G-0E18PQWKB3"
+  };
